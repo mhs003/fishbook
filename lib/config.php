ec9ecc0485fd8d1f1a1df58178fa6d73a4bbb448
mhs003 <49614333+mhs003@users.noreply.github.com>
2021-03-23 02:07:12 +0600

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-$site_name = "fishbook";
-$lock_admin_panel = true;
-$username = "admin";
-$password = "admin";
+
+//-- Start editing --
+$site_name = "fishbook"; // Site name, shows in admin panel
+$lock_admin_panel = true; // Set true if you want to lock admin panel, else false 
+$username = "admin"; // Admin panel login username
+$password = "admin"; // Admin panel login password
+
+//-- Do not Touch --
 $mup = md5($username.$password);
